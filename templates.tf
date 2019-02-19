f360f4ec1c87740b9482cf0b323eaf381bf5a22d
Josh Wolfer <mourne@gmail.com>
2019-02-19 16:32:17 -0600

    haproxy setup

---
+++ templates.tf
@@ -0,0 +1,10 @@
+data "template_file" "haproxyconf" {
+	template = "${file("${path.module}/config/haproxy.cfg.tpl")}"
+
+	var {
+		web1_priv_ip = "${digitalocean_droplet.web-1.ipv4_address_private}"
+		web2_priv_ip = "${digitalocean_droplet.web-2.ipv4_address_private}"
+		}
+}
+
+		
