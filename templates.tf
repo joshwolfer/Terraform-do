99f88e8a0a9e915db44ce0894e9a5405b9b61b85
Josh Wolfer <mourne@gmail.com>
2019-02-25 14:33:54 -0600

    fix var typo in template.tf

--- templates.tf
+++ templates.tf
@@ -1,10 +1,10 @@
 data "template_file" "haproxyconf" {
 	template = "${file("${path.module}/config/haproxy.cfg.tpl")}"
 
-	var {
+	vars = {
 		web1_priv_ip = "${digitalocean_droplet.web-1.ipv4_address_private}"
 		web2_priv_ip = "${digitalocean_droplet.web-2.ipv4_address_private}"
-		}
+	}
 }
 
 		
